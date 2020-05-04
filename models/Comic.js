187aa04512fbba187d424b070ad7052802f72a0c
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-05-04 04:49:38 -0700

    updated variables and names

--- models/Comic.js
+++ models/Comic.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
 
 // Using the Schema constructor, create a new UserSchema object
 // This is similar to a Sequelize model
-var ArticleSchema = new Schema({
+var ComicSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
@@ -19,14 +19,14 @@ var ArticleSchema = new Schema({
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  note: {
+  comment: {
     type: Schema.Types.ObjectId,
-    ref: "Note"
+    ref: "Comment"
   }
 });
 
 // This creates our model from the above schema, using mongoose's model method
-var Article = mongoose.model("Article", ArticleSchema);
+var Comic = mongoose.model("Comic", ComicSchema);
 
 // Export the Article model
-module.exports = Article;
+module.exports = Comic;
