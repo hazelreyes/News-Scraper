35bf709932ddac6c9a6e9962fe1e8c64147a76d0
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-05-05 22:29:38 -0700

    renamed files back to original names

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Comic: require("./Comic"),
-    Comment: require("./Comment")
+    Article: require("./Article"),
+    Note: require("./Note")
   };
   
--- server.js
+++ server.js
@@ -27,31 +27,31 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/comicScraper", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://theoatmeal.com/").then(function (response) {
+  axios.get("https://www.bbc.com/news/world/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("#comics_list a").each(function (i, element) {
+    $("a .gs-c-promo-heading").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this).children().attr("alt");
+      result.title = $(this).children("h3").text();
       result.link = $(this).attr("href");
 
       // Create a new Article using the `result` object built from scraping
-      db.Comic.create(result)
-        .then(function (dbComic) {
+      db.Article.create(result)
+        .then(function (dbArticle) {
           // View the added result in the console
-          console.log(dbComic);
+          console.log(dbArticle);
         })
         .catch(function (err) {
           // If an error occurred, log it
@@ -65,12 +65,12 @@ app.get("/scrape", function (req, res) {
 });
 
 // Route for getting all Articles from the db
-app.get("/comics", function (req, res) {
+app.get("/articles", function (req, res) {
   // Grab every document in the Articles collection
-  db.Comic.find({})
-    .then(function (dbComic) {
+  db.Article.find({})
+    .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbComic);
+      res.json(dbArticle);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
@@ -79,14 +79,14 @@ app.get("/comics", function (req, res) {
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.get("/comics/:id", function (req, res) {
+app.get("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Comic.findOne({ _id: req.params.id })
+  db.Article.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
-    .populate("comment")
-    .then(function (dbComic) {
+    .populate("note")
+    .then(function (dbArticle) {
       // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbComic);
+      res.json(dbArticle);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
@@ -95,22 +95,22 @@ app.get("/comics/:id", function (req, res) {
 });
 
 // Route for saving/updating an Article's associated Note
-app.post("/comics/:id", function (req, res) {
+app.post("/articles/:id", function (req, res) {
   // Create a new note and pass the req.body to the entry
-  db.Comment.create(req.body)
-    .then(function (dbComment) {
+  db.Note.create(req.body)
+    .then(function (dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Comic.findOneAndUpdate(
+      return db.Article.findOneAndUpdate(
         { _id: req.params.id },
-        { comment: dbComment._id },
+        { note: dbNote._id },
         { new: true }
       );
     })
-    .then(function (dbComic) {
+    .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
-      res.json(dbComic);
+      res.json(dbArticle);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
