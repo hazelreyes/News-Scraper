17bfa2b1b97339e8dda7d02006b15dc63daa953f
Hazel Reyes <hazelnoriereyes@gmail.com>
2020-05-07 13:58:10 -0700

    changed connection to mongodb

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI
+mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
 
 // Routes
 
